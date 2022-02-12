ac61776cf5de7d56bb9365d8d02eca2921b65a0d
Artem Dzuba <artemdzuba330@gmail.com>
2022-02-12 10:40:29 +0300

    ljbabv

--- git.ignore.py
+++ git.ignore.py
@@ -1,2 +1,10 @@
+<<<<<<< Updated upstream
 print('Игнорирование файлов')
 print('svhhavohav')
+=======
+
+
+print('srvpijpisveca')
+print('aecniiencnaciaxnncacinanicenae')
+sveknnvnvs
+>>>>>>> Stashed changes
